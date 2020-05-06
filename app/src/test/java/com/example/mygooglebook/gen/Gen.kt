ec90c2ca593861ade13ae4a342696db1a84b6ae5
pjshin3 <tlsvlfwp1@naver.com>
2020-05-06 22:49:05 +0900

    32
    1. 단위테스트
    - Gen을 통한 초기화 코드 추가
    - ViewModel 초기화 코드 추가

--- app/src/test/java/com/example/mygooglebook/gen/Gen.kt
+++ app/src/test/java/com/example/mygooglebook/gen/Gen.kt
@@ -1,22 +1,47 @@
 package com.example.mygooglebook.gen
 
 import arrow.core.Either
-import arrow.core.left
 import com.example.mygooglebook.remote.data.BookError
 import io.kotlintest.properties.Gen
+import com.example.mygooglebook.util.left
+import com.example.mygooglebook.util.right
 
 
 class SuggstionGenerator: Gen<Either<BookError,List<String>>>{
     override fun constants(): Iterable<Either<BookError, List<String>>> = emptyList()
 
-    override fun random(): Sequence<Either<BookError, List<String>>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun random(): Sequence<Either<BookError, List<String>>> = generateSequence {
+        genaerateEither(Gen.bool().random().first())
     }
 
     private fun genaerateEither(it: Boolean): Either<BookError,List<String>> {
-        TODO("")
+        return if(it){
+            left(Gen.bookError().random().first())
+        }else{
+            right((1..10).fold( emptyList()) { acc, _ -> acc + Gen.query().random().iterator().next() })
+        }
     }
 }
 
+class BookErrorGenerator : Gen<BookError> {
+    override fun constants(): Iterable<BookError> = emptyList()
+
+    override fun random(): Sequence<BookError> = generateSequence {
+        takeIf { Gen.bool().random().first() }
+            ?.let { BookError.EmptyResultError } ?: BookError.NetworkError
+    }
+}
+
+class QueryGenerator : Gen<String> {
+    override fun constants(): Iterable<String> = Gen.string().constants().filter{ it.isNotEmpty() }
+
+    override fun random(): Sequence<String> = Gen.string().random().filter { it.isNotEmpty() }
+}
+
+
 
 fun Gen.Companion.suggestion(): Gen<Either<BookError,List<String>>> = SuggstionGenerator()
+
+fun Gen.Companion.bookError(): Gen<BookError> = BookErrorGenerator()
+
+fun Gen.Companion.query(): Gen<String> = QueryGenerator()
--- app/src/test/java/com/example/mygooglebook/suggestion/SuggestionViewModelTest.kt
+++ app/src/test/java/com/example/mygooglebook/suggestion/SuggestionViewModelTest.kt
@@ -1,11 +1,37 @@
 package com.example.mygooglebook.suggestion
 
-import io.kotlintest.shouldBe
+import arrow.core.Either
+import com.example.mygooglebook.Seach.SuggestionViewModel
+import com.example.mygooglebook.gen.suggestion
+import com.example.mygooglebook.remote.data.BookError
+import com.example.mygooglebook.util.toFlowable
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.assertAll
 import io.kotlintest.specs.StringSpec
+import io.reactivex.Flowable
 
 class SuggestionViewModelTest :
     StringSpec({
         "Suggestion ViewModel should not trigger search for empty query" {
-            "hello".length shouldBe 6
+            assertAll(Gen.suggestion()) { suggestion ->
+                val viewmodel = givenSuggestionViewModel(suggestion)
+                val observer = viewmodel.state.toFlowable().test()
+
+                TODO("초기화한 뷰모델을 옵저브해서 값을 체크")
+
+            }
         }
+    })
+
+
+private fun SuggestionViewModel.inputQuery(input : String){
+    query.value = input
+    TODO()
+}
+
+private fun givenSuggestionViewModel(suggestion: Either<BookError,List<String>>): SuggestionViewModel =
+    SuggestionViewModel.invoke(mock{
+        on { searchSuggestion(any()) }
     })
